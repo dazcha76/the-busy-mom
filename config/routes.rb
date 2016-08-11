7c60016757bb67b0dc6c9fadb7f8547c66130f83
Danika <dazcha@hotmail.com>
2016-08-10 21:24:45 -0500

    Added signup page for new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   get '/' => 'home#index'
+
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
 
   get 'meals' => 'meals#index'
   get 'meals/:id' => 'meals#show'
