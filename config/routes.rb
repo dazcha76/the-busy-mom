188c8e31d5efada4f08080ee3a2c817753266554
Danika <dazcha@hotmail.com>
2016-08-14 12:59:38 -0500

    Added login page for registered users'

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/users/new' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get 'meals' => 'meals#index'
   get 'meals/:id' => 'meals#show'
 end
