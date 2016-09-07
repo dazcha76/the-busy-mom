3e2f2bb538fd01c0d60b92544fe50bf7d02347d0
Danika <dazcha@hotmail.com>
2016-09-06 20:29:41 -0500

    Added edit and delete routes to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,11 +13,35 @@ class RecipesController < ApplicationController
       prep_time: params['prep_time'],
       directions: params['directions']
     )
-    redirect_to '/recipes'
+    flash[:success] = 'Your recipe was successfully created!'
+    redirect_to '/recipes/#{recipe.id}'
   end
 
   def show
     @recipe = Recipe.find_by(id: params['id'])
     @ingredients = Ingredient.all.order(:name)
+  end
+
+  def edit
+    @recipe = Recipe.find_by(id: params['id'])
+  end
+
+  def update
+    @recipe = Recipe.find_by(id: params['id'])
+    @recipe.update(
+      name: params['name'],
+      image: params['image'],
+      prep_time: params['prep_time'],
+      directions: params['directions']
+    )
+    flash[:success] = 'Your recipe was successfully edited!'
+    redirect_to '/recipes/#{@recipe.id}'
+  end
+
+  def destroy
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    flash[:success] = 'Your recipe was successfully deleted!'
+    redirect_to '/recipes'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814215647) do
+ActiveRecord::Schema.define(version: 20160804013637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,8 @@ ActiveRecord::Schema.define(version: 20160814215647) do
   create_table "day_recipes", force: :cascade do |t|
     t.integer  "day_id"
     t.integer  "recipe_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "days", force: :cascade do |t|
